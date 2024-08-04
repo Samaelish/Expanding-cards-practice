7dea1da6004933d7665db13ce105b821794c4f5d
Samaelish <semnd007@gmail.com>
2024-08-04 12:52:10 +0300

    added comments to js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// array of urlArrays used to assign to panels
 let imageUrlArray = [
   'url("https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
   'url("https://images.unsplash.com/photo-1433086966358-54859d0ed716?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
@@ -5,6 +6,8 @@ let imageUrlArray = [
   'url("https://images.unsplash.com/photo-1482938289607-e9573fc25ebb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
   'url("https://images.unsplash.com/photo-1518495973542-4542c06a5843?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
 ]
+
+// array of panels (html elements)
 let panels = document.querySelectorAll('.panel')
 
 function assignCssProperty(elements, property, stringPropertiesArray) {
@@ -15,6 +18,8 @@ function assignCssProperty(elements, property, stringPropertiesArray) {
 }
 
 assignCssProperty(panels, 'backgroundImage', imageUrlArray)
+
+// adds a click action for user on panels so that panels expand and collapse
 
 panels.forEach(panel => {
   panel.addEventListener('click', () => {
